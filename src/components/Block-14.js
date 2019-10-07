c1e46407ced709d76ec003c0baadae29561022e5
Raigo Jerva <raigo.jerva@gmail.com>
2019-10-07 13:56:14 +0100

    add block-14

---
+++ src/components/Block-14.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+const wave = keyframes`
+  0%,
+  100% {
+    top: 0;
+  }
+
+  50% {
+    top: 70%;
+  }
+`;
+
+const Wrapper = styled.div`
+  background-color: #fff;
+  position: relative;
+  overflow: hidden;
+`;
+
+const Text = styled.span`
+  font-family: "Inter", sans-serif;
+  text-shadow: -1px -1px #000, 1px -1px #000, 1px 1px #000, -1px 1px #000;
+  font-size: 10rem;
+  color: #fff;
+  position: absolute;
+  left: ${p => p.xpos}rem;
+  animation: ${wave} 8s ease-in-out ${p => p.delay}s infinite;
+`;
+
+const BlockTwelve = () => {
+  const generateWaves = () => {
+    const waves = [];
+    for (let i = 1; i < 50; i += 1) {
+      waves.push(
+        <Text xpos={i} delay={0.2 * i} key={i}>{'<wave>'}</Text>,
+      );
+    }
+    return waves.reverse();
+  };
+  return (
+    <Wrapper>
+      {
+        generateWaves()
+      }
+    </Wrapper>
+  );
+};
+
+export default BlockTwelve;
