5dc4a85cc75be24f57418f269ec91e05f7162ca9
Raigo Jerva <raigo.jerva@gmail.com>
2020-06-20 21:11:51 +0300

    fix overflow bug, add jsconfig

--- src/pages/PageWrapper.js
+++ src/pages/PageWrapper.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
-import media from '../util/mediaQueries';
+import media from 'util/mediaQueries';
 
 const Wrapper = styled.div`
   display: grid;
-  grid-template-columns: 50vw 50vw;
+  grid-template-columns: 50% 50%;
   grid-auto-rows: 40rem;
   ${media.widelet`
     grid-template-columns: 100%;
