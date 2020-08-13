ffb11adb4664425991466358b0ceb0ee18a44f76
Raigo Jerva <raigo.jerva@gmail.com>
2020-08-13 22:22:55 +0300

    use translate3d

--- src/components/Block-2.js
+++ src/components/Block-2.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import styled, { keyframes } from 'styled-components';
+import React from "react";
+import styled, { keyframes } from "styled-components";
 
 const rotate = keyframes`
   from {
     transform:
       rotate(0deg)
-      translate(-80px);
+      translate3d(-80px, 0, 0);
   }
 
   to {
     transform:
       rotate(360deg)
-      translate(-80px);
+      translate3d(-80px, 0, 0);
   }
 `;
 
@@ -27,19 +27,12 @@ const Wrapper = styled.div`
 const Text = styled.span`
   font-size: 10rem;
   text-transform: uppercase;
-  font-family: 'Chela One', cursive;
+  font-family: "Chela One", cursive;
   letter-spacing: 10px;
   z-index: 1;
   color: #ead7de;
-  text-shadow:
-    1px 1px #fff,
-    2px 2px #fff,
-    3px 3px #fff,
-    4px 4px #fff,
-    5px 5px #fff,
-    6px 6px #fff,
-    7px 7px #fff,
-    8px 8px #fff;
+  text-shadow: 1px 1px #fff, 2px 2px #fff, 3px 3px #fff, 4px 4px #fff,
+    5px 5px #fff, 6px 6px #fff, 7px 7px #fff, 8px 8px #fff;
 `;
 
 const Circle = styled.div`
@@ -48,8 +41,15 @@ const Circle = styled.div`
   width: 1000rem;
   height: 1000rem;
   border-radius: 50rem;
-  background: repeating-radial-gradient(circle farthest-side at center center, transparent, transparent 2.5rem, #1d0073 2.5rem, #1d0073 5rem);
+  background: repeating-radial-gradient(
+    circle farthest-side at center center,
+    transparent,
+    transparent 2.5rem,
+    #1d0073 2.5rem,
+    #1d0073 5rem
+  );
   animation: ${rotate} 8s linear infinite;
+  transform: translate3d(0, 0, 0);
 `;
 
 const BlockTwo = () => (
--- src/components/Block-3.js
+++ src/components/Block-3.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import styled, { keyframes } from 'styled-components';
+import React from "react";
+import styled, { keyframes } from "styled-components";
 
 const rayAnimation = keyframes`
   from {
@@ -28,13 +28,19 @@ const Rays = styled.div`
   position: absolute;
   width: 1000rem;
   height: 1000rem;
-  background: repeating-conic-gradient(#fff 0%, #fff 2.5%, #2ac2ff 2.5%, #2ac2ff 5%);
+  background: repeating-conic-gradient(
+    #fff 0%,
+    #fff 2.5%,
+    #2ac2ff 2.5%,
+    #2ac2ff 5%
+  );
   animation: ${rayAnimation} 20s linear infinite;
+  transform: translate3d(0, 0, 0);
 `;
 
 const Text = styled.span`
   font-size: 10rem;
-  font-family: 'Pacifico', cursive;
+  font-family: "Pacifico", cursive;
   letter-spacing: 10px;
   z-index: 1;
   color: #fff3b2;
