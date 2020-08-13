a0640ed1d0ba87cb116772db21de7151f94178b9
Raigo Jerva <raigo.jerva@gmail.com>
2020-08-13 23:14:24 +0300

    finish page 1

--- src/pages/Page-1.js
+++ src/pages/Page-1.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import Wrapper from './PageWrapper';
-import BlockOne from '../components/Block-1';
-import BlockTwo from '../components/Block-2';
-import BlockThree from '../components/Block-3';
-import BlockFour from '../components/Block-4';
-import BlockFive from '../components/Block-5';
-import BlockSix from '../components/Block-6';
-import BlockSeven from '../components/Block-7';
-import BlockEight from '../components/Block-8';
-import BlockNine from '../components/Block-9';
-import BlockTen from '../components/Block-10';
+import React from "react";
+import BlockOne from "../components/Block-1";
+import BlockTen from "../components/Block-10";
+import BlockTwo from "../components/Block-2";
+import BlockThree from "../components/Block-3";
+import BlockFour from "../components/Block-4";
+import BlockFive from "../components/Block-5";
+import BlockSix from "../components/Block-6";
+import BlockSeven from "../components/Block-7";
+import BlockEight from "../components/Block-8";
+import BlockNine from "../components/Block-9";
+import Wrapper from "./PageWrapper";
 
 const PageOne = () => (
   <Wrapper>
