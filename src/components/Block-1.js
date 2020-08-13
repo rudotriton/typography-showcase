230598f96ecf849e2c4fa321029066d00be33da9
Raigo Jerva <raigo.jerva@gmail.com>
2020-08-13 20:15:47 +0300

    optimize block 1

--- src/components/Block-1.js
+++ src/components/Block-1.js
@@ -1,64 +1,63 @@
-import React from 'react';
-import styled, { keyframes } from 'styled-components';
+import React from "react";
+import styled, { keyframes } from "styled-components";
 
-const red = '#F40000';
-const green = '#04E762';
-const blue = '#2176FF';
-const yellow = '#FFBA00';
-const pink = '#DC0073';
-
-const zebraShadow = (size, colors) => {
-  let textShadow = '';
-  for (let i = 0; i < colors.length; i += 1) {
-    for (let j = 0; j < size; j += 1) {
-      const pos = (i) * size + j;
-      const shadow = `${pos}px ${pos}px ${colors[i]}, `;
-      textShadow += shadow;
-    }
-  }
-  textShadow = textShadow.slice(0, -2);
-  return textShadow;
-};
+const colors = ["#FFBA00", "#DC0073", "#2176FF", "#04E762", "#F40000"];
 
 const shadowAnimation = keyframes`
-  from,
+  0%,
+  9.9% {
+    transform: translate3d(0px, 0px, 0);
+    z-index:10;
+  }
+
+  10%,
   19.9% {
-    text-shadow:
-      ${zebraShadow(5,
-    [blue, pink, yellow, red, green,
-      blue, pink, yellow, red, green])};
+    transform: translate3d(5px, 5px, 0);
+    z-index:9;
   }
 
   20%,
-  39.9% {
-    text-shadow:
-      ${zebraShadow(5,
-    [green, blue, pink, yellow, red,
-      green, blue, pink, yellow, red])};
+  29.9% {
+    transform: translate3d(10px, 10px, 0);
+    z-index:8;
   }
 
+  30%,
+  39.9% {
+    transform: translate3d(15px, 15px, 0);
+    z-index:7;
+  }
   40%,
+  49.9% {
+    transform: translate3d(20px, 20px, 0);
+    z-index:6;
+  }
+  50%,
   59.9% {
-    text-shadow:
-      ${zebraShadow(5,
-    [red, green, blue, pink, yellow,
-      red, green, blue, pink, yellow])};
+    transform: translate3d(25px, 25px, 0);
+    z-index:5;
   }
 
   60%,
+  69.9% {
+    transform: translate3d(30px, 30px, 0);
+    z-index:4;
+  }
+  70%,
   79.9% {
-    text-shadow:
-      ${zebraShadow(5,
-    [yellow, red, green, blue, pink,
-      yellow, red, green, blue, pink])};
+    transform: translate3d(35px, 35px, 0);
+    z-index:3;
+  }
+  80%,
+  89.9% {
+    transform: translate3d(40px, 40px, 0);
+    z-index:2;
   }
 
-  80%,
-  to {
-    text-shadow:
-      ${zebraShadow(5,
-    [pink, yellow, red, green, blue,
-      pink, yellow, red, green, blue])};
+  90%,
+  100% {
+    transform: translate3d(45px, 45px, 0);
+    z-index:1;
   }
 `;
 
@@ -67,22 +66,31 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
   background-color: #fff;
+  z-index: -20;
   overflow: hidden;
   position: relative;
 `;
 
 const Text = styled.span`
   font-size: 10rem;
-  font-family: 'Parisienne', cursive;
+  font-family: "Parisienne", cursive;
+  position: absolute;
   letter-spacing: 10px;
-  z-index: 1;
-  color: transparent;
-  animation: ${shadowAnimation} 0.75s linear infinite;
+  color: ${(p) => p.color};
+  text-shadow: 1px 1px ${(p) => p.color}, 2px 2px ${(p) => p.color},
+    3px 3px ${(p) => p.color}, 4px 4px ${(p) => p.color},
+    5px 5px ${(p) => p.color};
+  animation: ${shadowAnimation} 2s linear ${(p) => p.delay}s infinite;
+  transform: translate3d(0, 0, 0);
 `;
 
 const BlockOne = () => (
   <Wrapper>
-    <Text>Love</Text>
+    {[...colors, ...colors].map((color, idx) => (
+      <Text key={`${idx + color}`} color={color} delay={idx * 0.2}>
+        Love
+      </Text>
+    ))}
   </Wrapper>
 );
 
