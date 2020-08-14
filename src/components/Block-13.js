969e9ae12b6cb7dc5b92780f57d94ca8f7940676
Raigo Jerva <raigo.jerva@gmail.com>
2020-08-14 10:30:45 +0300

    optimize 13

--- src/components/Block-13.js
+++ src/components/Block-13.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import styled, { keyframes } from 'styled-components';
+import React from "react";
+import styled, { keyframes } from "styled-components";
 
 const rotate = keyframes`
   0%,
@@ -16,76 +16,56 @@ const rotate = keyframes`
     transform: rotate(360deg);
   }
 `;
-const sunMoonRotate = keyframes`
-  0%,
-  40% {
-    transform: rotate(0deg) translate(-50%, -50%);
-  }
-
-  50%,
-  90% {
-    transform: rotate(180deg) translate(50%, 50%);
-  }
-
-  100% {
-    transform: rotate(360deg) translate(-50%, -50%);
-  }
-`;
 
 const smoothOcean = keyframes`
   0%,
   40% {
-    background: #1B46EF;
+    opacity: 0;
   }
 
   50%,
   90% {
-    background: #030733;
+    opacity: 1;
   }
 
   100% {
-    background: #1B46EF;
+    opacity: 0;
   }
 `;
 
 const Wrapper = styled.div`
   position: relative;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  display: grid;
+  place-items: center;
   overflow: hidden;
 `;
 
 const Horizon = styled.div`
-  background:
-    linear-gradient(
-      180deg,
-      #fe5f75 0%,
-      #fc9842 50%,
-      #0064ad 50.01%,
-      #000b72 100%
-    );
-  height: 100%;
-  width: 100%;
+  background: linear-gradient(
+    180deg,
+    #fe5f75 40%,
+    #fc9842 50%,
+    #0064ad 50.01%,
+    #000b72 60%
+  );
+  height: 400%;
+  width: 400%;
+  display: grid;
+  place-items: center;
   animation: ${rotate} 8s ease-in-out infinite;
 `;
 
 const SunMoon = styled.div`
-  background:
-    linear-gradient(
-      180deg,
-      #ff9a15 9.17%,
-      #ff7715 22.89%,
-      #f41d1d 50%,
-      #fffdea 50.01%
-    );
+  background: linear-gradient(
+    180deg,
+    #ff9a15 9.17%,
+    #ff7715 22.89%,
+    #f41d1d 50%,
+    #fffdea 50.01%
+  );
   height: 20rem;
   width: 20rem;
-  position: absolute;
-  left: 50%;
-  top: 50%;
   border-radius: 10rem;
-  animation: ${sunMoonRotate} 8s ease-in-out infinite;
 `;
 
 const Sea = styled.div`
@@ -93,27 +73,35 @@ const Sea = styled.div`
   height: 50%;
   width: 100%;
   top: 50%;
-  background: #e5326a;
-  animation: ${smoothOcean} 8s ease-in-out infinite;
+  background-color: #1b46ef;
+  &:after {
+    content: "";
+    background-color: #030733;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    animation: ${smoothOcean} 8s ease-in-out infinite;
+  }
 `;
 
 const Text = styled.span`
-  font-family: 'Pacifico', cursive;
-  mix-blend-mode: exclusion;
+  font-family: "Pacifico", cursive;
+  mix-blend-mode: difference;
   position: absolute;
   left: 50%;
   top: 50%;
   transform: translate(-50%, -65%);
   font-size: 10rem;
   z-index: 10;
-  color: #FF2100;
+  color: #ff2100;
 `;
 
 const BlockThirteen = () => (
   <Wrapper>
-    <Horizon />
+    <Horizon>
+      <SunMoon />
+    </Horizon>
     <Text>Change</Text>
-    <SunMoon />
     <Sea />
   </Wrapper>
 );
