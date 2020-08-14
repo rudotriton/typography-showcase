507308b931fb287624a29bda598bbf2263f6f267
Raigo Jerva <raigo.jerva@gmail.com>
2020-08-14 13:52:10 +0300

    optimize 18

--- src/components/Star.js
+++ src/components/Star.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import styled from 'styled-components';
+/* eslint-disable react/prop-types */
+import React from "react";
+import styled from "styled-components";
 
 const Svg = styled.svg`
-  animation: ${(p) => p.s}s ${(p) => p.a} 0.${(p) => p.d}s infinite ease-in-out;
+  animation: ${(p) => p.a} ${(p) => p.s}s linear ${(p) => p.d}s infinite;
   position: absolute;
   height: 100px;
   width: 100px;
   left: ${(props) => props.x}%;
   top: ${(props) => props.y}%;
-  transform: translate(-50%, -50%) scale(0);
   fill: white;
+  transform: translate3d(-50%, -50%, 0) scale(0);
 `;
 
-const Star = ({
-  // eslint-disable-next-line react/prop-types
-  x, y, animation, speed = 7, delay = 0,
-}) => (
+const Star = ({ x, y, animation, speed = 7, delay = 0 }) => (
   <Svg x={x} y={y} a={animation} s={speed} d={delay} viewBox="0 0 100 100">
     <path d="M50,0 C50,37 63,50 100,50 C63,50 50,63 50,100 C50,63 37,50 0,50 C37,50 50,37 50,0 Z" />
   </Svg>
