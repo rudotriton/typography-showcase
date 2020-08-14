1702cc1236ae0c2c77f8b20d153549fa33b773c1
Raigo Jerva <raigo.jerva@gmail.com>
2020-08-14 11:46:59 +0300

    optimize 17

--- src/components/Block-17.js
+++ src/components/Block-17.js
@@ -1,18 +1,16 @@
-import React from 'react';
-import styled, { keyframes } from 'styled-components';
+import React from "react";
+import styled, { keyframes } from "styled-components";
 
 const Appear = keyframes`
   0%,
   10%,
   90%,
   100% {
-    text-shadow: none;
+    opacity: 0;
   }
   
   50% {
-    text-shadow:
-    -0.2rem -0.2rem 1rem rgba(255, 255, 255, 0.9),
-    0.2rem 0.2rem 1rem rgba(0, 0, 0, 0.15);
+    opacity: 1;
   }
 `;
 
@@ -24,10 +22,11 @@ const Wrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  z-index: -10;
 `;
 
 const Text = styled.span`
-  font-family: 'Inter', sans-serif;
+  font-family: "Inter", sans-serif;
   font-weight: 700;
   font-size: 10rem;
   line-height: 8rem;
@@ -35,15 +34,37 @@ const Text = styled.span`
 `;
 
 const Letter = styled.span`
+  opacity: 0;
   animation: ${Appear} 6s ease-in-out ${(p) => p.delay}s infinite;
+  &:after {
+    filter: blur(5px);
+    position: absolute;
+    z-index: -1;
+    content: ${(p) => `"${p.letter}"`};
+    transform: translate3d(-104%, -5%, 0);
+    color: rgba(255, 255, 255, 0.9);
+  }
+  &:before {
+    filter: blur(5px);
+    position: absolute;
+    z-index: -1;
+    content: ${(p) => `"${p.letter}"`};
+    transform: translate3d(3%, 4%, 0);
+    color: rgba(0, 0, 0, 0.15);
+  }
 `;
 
 const BlockSeventeen = () => {
-  const createLetters = (text, delay) => text.split('').map((letter, i) => <Letter key={`${i + letter}`} delay={(delay + i) * 0.2}>{letter}</Letter>);
+  const createLetters = (text, delay) =>
+    text.split("").map((letter, i) => (
+      <Letter key={`${i + letter}`} letter={letter} delay={(delay + i) * 0.2}>
+        {letter}
+      </Letter>
+    ));
   return (
     <Wrapper>
-      <Text>{createLetters('Les Yeux', 0)}</Text>
-      <Text>{createLetters('Sans Visage', 11)}</Text>
+      <Text>{createLetters("Les Yeux", 0)}</Text>
+      <Text>{createLetters("Sans Visage", 11)}</Text>
     </Wrapper>
   );
 };
