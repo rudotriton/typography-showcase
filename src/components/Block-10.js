8d5f9dc9aa9f05e7739dcd284de5a408eab19818
Raigo Jerva <raigo.jerva@gmail.com>
2020-09-24 12:35:09 +0300

    update glitch effect

--- src/components/Block-10.js
+++ src/components/Block-10.js
@@ -10,7 +10,7 @@ const red = keyframes`
   78%,
   88%,
   100% {
-    transform: translate3d(-50%, -50%, 0);
+    transform: translate3d(0, 0, 0);
   }
 
   5%,
@@ -20,11 +20,30 @@ const red = keyframes`
   72%,
   84%,
   95% {
-    transform: translate3d(-48%, -52%, 0);
+    transform: translate3d(0.07em, -0.07em, 0);
   }
 `;
 
 const blue = keyframes`
+  0%,
+  7%,
+  40%,
+  61%,
+  88%,
+  100% {
+    transform: translate3d(-100%, 0, 0);
+  }
+
+  5%,
+  15%,
+  47%,
+  72%,
+  95% {
+    transform: translate3d(calc(-100% - 0.07em), 0.07em, 0);
+  }
+`;
+
+const first = keyframes`
   0%,
   7%,
   40%,
@@ -39,7 +58,26 @@ const blue = keyframes`
   47%,
   72%,
   95% {
-    transform: translate3d(-52%, -48%, 0);
+    transform: translate3d(-52%, -52%, 0);
+  }
+`;
+
+const last = keyframes`
+  0%,
+  7%,
+  40%,
+  61%,
+  88%,
+  100% {
+    transform: translate3d(-50%, -50%, 0);
+  }
+
+  5%,
+  15%,
+  47%,
+  72%,
+  95% {
+    transform: translate3d(-48%, -50%, 0);
   }
 `;
 
@@ -47,7 +85,7 @@ const Wrapper = styled.div`
   background-color: #000;
   position: relative;
 
-  & > span {
+  & span {
     font-family: "Inter", sans-serif;
     font-size: 10rem;
     font-weight: 900;
@@ -55,32 +93,50 @@ const Wrapper = styled.div`
     text-transform: uppercase;
     top: 50%;
     left: 50%;
+    transform: translate3d(-50%, -50%, 0);
     mix-blend-mode: screen;
+    color: #0f0;
 
-    &.red {
+    &::after {
+      mix-blend-mode: screen;
+      content: "ilomilo";
+      position: absolute;
+      color: #00f;
+      transform: translate3d(calc(-100% - 0.07em), 0.07em, 0);
+      animation: ${blue} 1.5s linear infinite;
+    }
+
+    &::before {
+      mix-blend-mode: screen;
+      content: "ilomilo";
+      position: absolute;
       color: #f00;
-      transform: translate3d(-48%, -52%, 0);
+      transform: translate3d(0.07em, -0.07em, 0);
       animation: ${red} 1.5s linear infinite;
     }
 
-    &.green {
-      color: #0f0;
-      transform: translate3d(-50%, -50%, 0);
+    &.first {
+      transform: translate3d(-52%, -52%, 0);
+      clip-path: polygon(0 0, 100% 0, 100% 50%, 0 50%);
+      animation: ${first} 1s linear infinite;
     }
-
-    &.blue {
-      color: #00f;
-      transform: translate3d(-52%, -48%, 0);
-      animation: ${blue} 1.5s linear infinite;
+    &.last {
+      transform: translate3d(-48%, -44%, 0);
+      clip-path: polygon(0 50%, 100% 50%, 100% 100%, 0 100%);
+      animation: ${last} 2s linear infinite;
     }
   }
 `;
 
 const BlockTen = () => (
   <Wrapper>
-    <span className="red">ilomilo</span>
-    <span className="green">ilomilo</span>
-    <span className="blue">ilomilo</span>
+    <span className="first" aria-hidden="true">
+      ilomilo
+    </span>
+    <span>ilomilo</span>
+    <span className="last" aria-hidden="true">
+      ilomilo
+    </span>
   </Wrapper>
 );
 
